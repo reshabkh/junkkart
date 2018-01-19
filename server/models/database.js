493b3af65bfe9690b09db372f316558647c4cb4c
Reshab Khandelwal <reshab>
2018-01-19 15:53:04 +0530

    setup app for heroku

--- server/models/database.js
+++ server/models/database.js
@@ -8,12 +8,16 @@ var dbConfig = {
     password: '',
     database: 'junkkart',
     charset: 'utf8'
-  } || {
-    host: 'cig4l2op6r0fxymw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-    user: 'mv955787qfjmxxqe',
-    password: 'kw87pjc8n0w2hfag',
-    database: 'jba1cx3fopr287xn',
-    charset: 'utf8'
+  }
+}
+var dbConfig = {
+  client: 'mysql',
+ connection :{
+  host: 'cig4l2op6r0fxymw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+  user: 'mv955787qfjmxxqe',
+  password: 'kw87pjc8n0w2hfag',
+  database: 'jba1cx3fopr287xn',
+  charset: 'utf8'
   }
 }
 
